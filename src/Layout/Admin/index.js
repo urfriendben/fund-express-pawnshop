0b4b190e563b8f0c31aefdb4e50001c8af63b4b7
Loi Fuh Chang <fuh_chang@Hotmail.com>
2017-10-15 19:18:21 +0800

    add back menu

--- src/Layout/Admin/index.js
+++ src/Layout/Admin/index.js
@@ -5,18 +5,32 @@ import './index.css';
 import Dashboard from './Component/Dashboard';
 import Pawner from './Component/Pawner';
 export default class Admin extends Component {
-  
+
   render() {
     return (
       <div className="layout-holder">
-        
+      <div className="ui menu-sidebar sidebar stackable bottom four item labelled icon menu">
+        <div className="item">
+        </div>
+        <a className="item">
+          <i className="id badge icon"></i>
+          CUSTOMERS
+        </a>
+        <a className="item">
+          <i className="id tasks icon"></i>
+          TRANSACTIONS
+        </a>
+        <a className="item">
+          <i className="bar chart icon"></i>
+          ANALYTICS
+        </a>
+      </div>
         <Switch>
           <Route exact path="/new" component={Dashboard} />
           <Route exact path="" component={Pawner} />
         </Switch>
-        
+
       </div>
     );
   }
 }
-
