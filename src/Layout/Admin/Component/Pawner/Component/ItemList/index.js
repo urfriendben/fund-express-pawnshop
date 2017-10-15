0700b5a90feabb973c41d68b5151a069a5f6e92a
Loi Fuh Chang <fuh_chang@Hotmail.com>
2017-10-15 16:18:07 +0800

    add item table and adding of item

--- src/Layout/Admin/Component/Pawner/Component/ItemList/index.js
+++ src/Layout/Admin/Component/Pawner/Component/ItemList/index.js
@@ -1,55 +1,84 @@
-import React from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import {Grid, Segment, Button, Checkbox, Icon, Table, Input  } from 'semantic-ui-react'
 
-const TableExamplePagination = () => (
-  <Table celled>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Header</Table.HeaderCell>
-        <Table.HeaderCell>Header</Table.HeaderCell>
-        <Table.HeaderCell>Header</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+class ItemList extends Component {
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>
-          <Label ribbon>First</Label>
-        </Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-        <Table.Cell>Cell</Table.Cell>
-      </Table.Row>
-    </Table.Body>
 
-    <Table.Footer>
-      <Table.Row>
-        <Table.HeaderCell colSpan='3'>
-          <Menu floated='right' pagination>
-            <Menu.Item as='a' icon>
-              <Icon name='left chevron' />
-            </Menu.Item>
-            <Menu.Item as='a'>1</Menu.Item>
-            <Menu.Item as='a'>2</Menu.Item>
-            <Menu.Item as='a'>3</Menu.Item>
-            <Menu.Item as='a'>4</Menu.Item>
-            <Menu.Item as='a' icon>
-              <Icon name='right chevron' />
-            </Menu.Item>
-          </Menu>
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Footer>
-  </Table>
-)
+  constructor (props) {
+    super(props);
+    this.state = {
+      toggle: false,
+      rows: [{name:"",weight:"",type:"",price:""}]
+    }
+     this.addRow = this.addRow.bind(this);
+  }
 
-export default TableExamplePagination
+  addRow(){
+     var rows = this.state.rows;
+     rows.push({name:this.itemName.inputRef.value,weight:this.itemWeight.inputRef.value,type:this.itemType.inputRef.value,price:this.itemPrice.inputRef.value});
+     this.setState({rows:rows});
+    }
+
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Grid.Column mobile={16} tablet={8} computer={3}>
+            space1
+          </Grid.Column>
+          <Grid.Column mobile={48} tablet={16} computer={10}>
+          <Table celled striped>
+              <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell >Remove/Add Item</Table.HeaderCell>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Weight</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+                <Table.HeaderCell>Price</Table.HeaderCell>
+              </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                   {this.state.rows.map((data) => {return (
+                     <Table.Row>
+                     <Table.Cell><Button basic color='red'>
+                       <Icon name='remove' /> No
+                     </Button></Table.Cell>
+                     <Table.Cell>{data.name}</Table.Cell>
+                     <Table.Cell>{data.weight}</Table.Cell>
+                     <Table.Cell>{data.type}</Table.Cell>
+                     <Table.Cell>{data.price}</Table.Cell>
+                     </Table.Row>);})}
+              </Table.Body>
+              <Table.Footer full-Width>
+                <Table.Row>
+                <Table.Cell>
+                   <Button primary size='mid' onClick={this.addRow} >ADD</Button>
+                 </Table.Cell>
+                <Table.Cell>
+                    <Input focus ref={input => this.itemName = input} placeholder='Enter Item Name' />
+                 </Table.Cell>
+                 <Table.Cell>
+                    <Input focus  ref={input => this.itemWeight = input} placeholder='Enter Item Weight' />
+                  </Table.Cell>
+                  <Table.Cell>
+                     <Input focus  ref={input => this.itemType = input} placeholder='Enter Item Type' />
+                   </Table.Cell>
+                   <Table.Cell>
+                    <Input focus ref={input => this.itemPrice = input} placeholder='Enter Item Price' />
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
+          </Grid.Column>
+          <Grid.Column mobile={16} tablet={8} computer={3}>
+            space 2
+          </Grid.Column>
+        </Grid>
+
+      </div>
+    );
+  }
+}
+
+
+export default ItemList
